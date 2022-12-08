9e46b34c9407213b658239e45ae985acbd5ab8ef
GetReqt05 <jmaucher05@icloud.com>
2022-12-08 15:00:56 +0100

    Spiel 2048 programmiert, HTML -> HTML, CSS, JS neuer Commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var size = 4// Spiel fertig
-var htmlElements;
-var cells;
+let size = 4;  // Spiel fertig
+let htmlElements;
+let cells;
 
 function FeldErstellen() {
     if (htmlElements){
         return;
     }
     htmlElements = [];
-    var table = document.getElementById('field')
+    let table = document.getElementById('field')
     for (var y = 0; y < size; y++) {
-        var tr = document.createElement('tr')
-        var trElements = [];
-        for (var x = 0; x < size; x++) {
-            var td = document.createElement("td")
+        let tr = document.createElement('tr')
+        let trElements = [];
+        for (let x = 0; x < size; x++) {
+            let td = document.createElement("td")
             td.setAttribute('class', 'cell');
             tr.appendChild(td);
             trElements.push(td)
@@ -23,12 +23,12 @@ function FeldErstellen() {
 }
 function ZellenErstellen() {
     cells = [];
-    for( var y = 0; y < size; y++) {
+    for( let y = 0; y < size; y++) {
         cells.push(new Array(size).fill(0));
     }
 }
 function GeneriereLeereZelle() {
-    var x,y;
+    let x,y;
     do {
         x = Math.floor(Math.random() * size), y = Math.floor(Math.random() * size);
         if (cells [y][x] == 0){
@@ -38,15 +38,15 @@ function GeneriereLeereZelle() {
     }while (true)
 }
 function draw() {
-    for (var y = 0; y < size; y++){
-        for (var x = 0; x < size; x++){
-            var td = htmlElements[y][x];
-            var v = cells[y][x];
+    for (let y = 0; y < size; y++){
+        for (let x = 0; x < size; x++){
+            let td = htmlElements[y][x];
+            let v = cells[y][x];
             td.innerHTML = v == 0 ? '' : String(v);
             if (v == 0) {
                 td.setAttribute('style', 'background-color: white');
             } else {
-                var h = 20 + 24 * Math.log2(2048 / v);
+                let h = 20 + 24 * Math.log2(2048 / v);
                 td.setAttribute('style', 'background-color: hsl(' + h + ', 100%, 50%)');
             }
         }
@@ -60,7 +60,7 @@ function Bewegen(array, size) {
 
     array = filterEmpty(array)
     if (array.length > 0) {
-        for (var i = 0; i < array.length - 1; i++) {
+        for (let i = 0; i < array.length - 1; i++) {
             if (array[i] == array [i + 1]) {
                 array[i] *= 2;
                 array[i + 1] = 0;
@@ -74,9 +74,9 @@ function Bewegen(array, size) {
     return array;
 }
 function NachLinksVerschieben() {
-    var changed = false;
-    for (var y = 0; y < size; y++) {
-        var alt= Array.from(cells[y]);
+    let changed = false;
+    for (let y = 0; y < size; y++) {
+        let alt= Array.from(cells[y]);
         cells[y] = Bewegen(cells[y], size);
         changed = changed || (cells[y].join(',') != old.join(','));
     }
@@ -90,16 +90,16 @@ function Tauschen(x1,y1,x2,y2) {
 }
 
 function Spiegeln() {
-    for( var y = 0; y< size; y++) {
-        for (var xLeft = 0, xRight = size - 1; xLeft < xRight; xLeft++, xRight--) {
+    for( let y = 0; y< size; y++) {
+        for (let xLeft = 0, xRight = size - 1; xLeft < xRight; xLeft++, xRight--) {
             Tauschen(xLeft, y, xRight, y);
         }
     }
 }
 
 function Umsetzen() {
-    for (var y =  0; y < size; y++) {
-        for (var x = 0; x < y; x++) {
+    for (let y =  0; y < size; y++) {
+        for (let x = 0; x < y; x++) {
             Tauschen(x, y, y, x);//nochmal commiten und pushen
         }
     }
@@ -112,35 +112,35 @@ function NachLinksBewegen() {
 
 function NachRechtsBewegen() {
     Spiegeln();
-    var changed = NachRechtsBewegen();
+    let changed = NachRechtsBewegen();
     Spiegeln();
     return changed;
 }
 function NachObenBewegen() {
     Umsetzen();
-    var changed = NachLinksBewegen();
+    let changed = NachLinksBewegen();
     Umsetzen();
     return changed;
 }
 
 function NachUntenBewegen() {
     Umsetzen();
-    var changed = NachRechtsBewegen();
+    let changed = NachRechtsBewegen();
     Umsetzen();
     return changed;
 }
 
 function SpielEnde() {
-    for (var y = 0; y < size ;y++) {
-        for (var x = 0; x < size; x++) {
+    for (let y = 0; y < size ;y++) {
+        for (let x = 0; x < size; x++) {
             if (cells[y][x] == 0) {
                 return false;
             }
         }
     }
-    for (var y = 0; y < size - 1; y++) {
-        for (var x = 0; x < size; x++) {
-            var c = cells[y][x]
+    for (let y = 0; y < size - 1; y++) {
+        for (let x = 0; x < size; x++) {
+            let c = cells[y][x]
             if (c != 0 && (c == cells[y + 1][x] || c == cells[y][x + 1])) {
                 return false
             }
@@ -149,8 +149,8 @@ function SpielEnde() {
     return true;
 }
 document.addEventListener('keydown', function(e) {
-    var code = e.keyCode;
-    var ok;
+    let code = e.keyCode;
+    let ok;
     switch (code) {
         case 40: ok = NachUntenBewegen(); break;
         case 38: ok = NachObenBewegen(); break;
